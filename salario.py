563cd6da1f55afcf48142d83934375067a8ddd8e
Ariel A. Barcelos <38111883+sabaoempo@users.noreply.github.com>
2018-05-29 19:05:25 -0300

    Create salario.py

---
+++ salario.py
@@ -0,0 +1,4 @@
+s_hora=int(input("Quantia recebida por hora(somente números): "))
+t_hora=int(input("Horas trabalhadas por mês(somente números): "))
+mes=input("Mês: ")
+print(s_hora*t_hora,"reais recebidos no mês de",mes)
